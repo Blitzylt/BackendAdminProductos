0d855e42b3f98896cff64af7b0d1d48020348118
Blitzylt <joe1ito20tn@gmail.com>
2025-10-26 12:13:11 -0500

    Se implemento en el controlador mas acciones [Eliminar y editar] para el administrador

--- spring-ecommerce/src/main/java/com/productos/ecommerce/spring_ecommerce/controller/ProductoController.java
+++ spring-ecommerce/src/main/java/com/productos/ecommerce/spring_ecommerce/controller/ProductoController.java
@@ -6,10 +6,12 @@ import com.productos.ecommerce.spring_ecommerce.model.Producto;
 import com.productos.ecommerce.spring_ecommerce.model.Usuario;
 import com.productos.ecommerce.spring_ecommerce.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/productos")
@@ -25,6 +27,46 @@ public class ProductoController {
         return productoService.findAll();
     }
 
+    //Obtener producto por id
+
+    @GetMapping("/editar/{id}")
+    public ResponseEntity<Producto> editar(@PathVariable Integer id) {
+        Optional<Producto> producto = productoService.get(id);
+        return producto.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<String> actualizarProducto(
+            @PathVariable Integer id,
+            @RequestParam("nombre") String nombre,
+            @RequestParam("descripcion") String descripcion,
+            @RequestParam("cantidad") int cantidad,
+            @RequestParam("precio") double precio
+    ) {
+        Optional<Producto> optionalProducto = productoService.get(id);
+        if (optionalProducto.isPresent()) {
+            Producto producto = optionalProducto.get();
+            producto.setNombre(nombre);
+            producto.setDescripcion(descripcion);
+            producto.setCantidad(cantidad);
+            producto.setPrecio(precio);
+            productoService.save(producto);
+            return ResponseEntity.ok("Producto actualizado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado.");
+        }
+    }
+
+    //Eliminar producto
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Integer id) {
+        productoService.delete(id);
+        return ResponseEntity.ok("Producto eliminado correctamente");
+    }
+
+
     @PostMapping("/crear")
     public ResponseEntity<String> crearProducto(
             @RequestParam("nombre") String nombre,
@@ -37,7 +79,7 @@ public class ProductoController {
 
         Producto producto = new Producto();
         producto.setNombre(nombre);
-        producto.setDescripcion(descripcion); // usa acento si tu atributo lo tiene
+        producto.setDescripcion(descripcion);
         producto.setCantidad(cantidad);
         producto.setPrecio(precio);
         producto.setUsuario(usuario);
